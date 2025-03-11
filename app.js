ef0d9b5a645cec16dc3f10cd31afb8b91c0e2d1b
Austen Stone <austenstone@github.com>
2025-03-11 14:18:19 -0400

    test123

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ var products = require('./routes/products');
 
 var app = express();
 
-const secret = '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';
-
 // config second logger
 log4js.loadAppender('file');
 //log4js.addAppender(log4js.appenders.console());
